589e51d3470836baa89bd046258dfe16b8959976
sinadjanMJ <sinadjanmichaeljay@gmail.com>
2024-02-21 15:23:30 +0800

    task#8 employee history

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Modal, Row, Table, Form, Input } from "reactstrap";
+import EmployeeHistory from "./components/EmpHistory";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <EmployeeHistory toggled={modal} untoggle={toggle}></EmployeeHistory>
 
-      <div>
+      {
+        /* <div>
         <FontAwesomeIcon icon={faCoffee} />
+        
         <span> This is a coffee icon.</span>
-      </div>
+      </div> */
+
+        <Button
+          color="info"
+          onClick={() => {
+            toggle();
+          }}
+        >
+          View Employment History
+        </Button>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
