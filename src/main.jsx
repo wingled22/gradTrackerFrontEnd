cf2caa0bb71fa62b1e6841328ae7f2b571244014
Lugubriszxc <@Lugubriszxc>
2024-02-21 14:06:15 +0800

    https://github.com/wingled22/gradTrackerFrontEnd/issues/4 Added accordion with lacking of style

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './index.css'
+import AccordionList from './AccordionList.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
