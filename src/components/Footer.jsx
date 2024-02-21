29c314fd6a0bc73c811ecfd0128d711db3a33f77
onin sama <ninoabao@gmail.com>
2024-02-21 12:15:49 +0800

    Partially finnished the navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../assets/css/Footer.css";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <p className="text-center mt-3">alumnitracker.com 2024-2025</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
