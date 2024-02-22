c7023fcc5a12bc27240da95f70362133252a30e1
stewart065 <clientstewart21@gmail.com>
2024-02-22 08:58:00 +0800

    addded buttons

--- src/Bttons.jsx
+++ src/Bttons.jsx
@@ -6,7 +6,7 @@ const Bttons = () => {
   return (
     <>
       <Button color='info' style={{ marginLeft: '420px', marginTop: "-350px" }}>Create</Button>
-<Button color='danger' style={{ marginLeft: '10px', marginTop: "-350px" }}>Delete</Button>
+      <Button color='danger' style={{ marginLeft: '10px', marginTop: "-350px" }}>Delete</Button>
 
       
     </>
